9aac5d8b9dcc3aa1ad86d3fb8f53c0c5dffdd378
Hwabum <kazz9000@gmail.com>
2023-12-12 07:57:08 +0900

    asda

--- components/GoalInput.js
+++ components/GoalInput.js
@@ -24,8 +24,9 @@ function GoalInput(props) {
     <Modal visible={props.visible} animationType="slide">
       <View style={styles.inputContainer}>
         <Image
-          style={styles.image} 
-          source={require('../assets/images/goal.png')}/>
+          style={styles.image}
+          source={require("../assets/images/goal.png")}
+        />
         <TextInput
           style={styles.textInput}
           placeholder="Your course goal!"
@@ -34,10 +35,10 @@ function GoalInput(props) {
         />
         <View style={styles.buttonContainer}>
           <View style={styles.button}>
-            <Button title="Add Goal" onPress={addGoalHandler} color="#5e0acc"/>
+            <Button title="Add Goal" onPress={addGoalHandler} color="#5e0acc" />
           </View>
           <View style={styles.button}>
-            <Button title="Cancel" onPress={props.onCancel} color="#f31282"/>
+            <Button title="Cancel" onPress={props.onCancel} color="#f31282" />
           </View>
         </View>
       </View>
@@ -53,18 +54,18 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     padding: 16,
-    backgroundColor: '#311b6b'
+    backgroundColor: "#311b6b",
   },
   image: {
     width: 100,
     height: 100,
-    margin: 20
-  },  
+    margin: 20,
+  },
   textInput: {
     borderWidth: 1,
     borderColor: "#e4d0ff",
     backgroundColor: "#e4d0ff",
-    color: '#120438',
+    color: "#120438",
     borderRadius: 6,
     width: "100%",
     padding: 8,
